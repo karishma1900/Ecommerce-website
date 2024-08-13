2d05d21d5e6e2ebf2d28850d0c792084f79e2844
Karishma's Classes <156506602+karishma1900@users.noreply.github.com>
2024-08-13 15:40:26 +0530

    Update ShopContext.jsx

--- New folder/src/Context/ShopContext.jsx
+++ New folder/src/Context/ShopContext.jsx
@@ -16,7 +16,7 @@ const ShopContextProvider = (props) =>{
     const [all_product, setAll_product] = useState([]);
     const [cartItems,setCartItems] = useState(getDefaultCart());
     useEffect(()=>{
-        fetch('http://localhost:4000/allproducts')
+        fetch('https://ecommerce-website-xhhx.onrender.com/allproducts')
         .then((response)=>response.json())
         .then((data)=>setAll_product(data))
     },[])
@@ -25,7 +25,7 @@ const ShopContextProvider = (props) =>{
     const addToCart = (itemId) =>{
           setCartItems((prev) =>({...prev,[itemId]:prev[itemId]+1}));
         if(localStorage.getItem('auth-token')){
-            fetch('http://localhost:4000/addtocart',{
+            fetch('https://ecommerce-website-xhhx.onrender.com/addtocart',{
                 method:'POST',
                 headers:{
                     Accept:'application/form-data',
@@ -42,7 +42,7 @@ const ShopContextProvider = (props) =>{
     const removeFromCart = (itemId) =>{
         setCartItems((prev) =>({...prev,[itemId]:prev[itemId]-1}));
         if(localStorage.getItem('auth-token')){
-            fetch('http://localhost:4000/removefromcart',{
+            fetch('https://ecommerce-website-xhhx.onrender.com/removefromcart',{
                 method:'POST',
                 headers:{
                     Accept:'application/form-data',
